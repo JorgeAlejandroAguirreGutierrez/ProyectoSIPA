ca168ebede72a8687bf1ae7e15860873462134f5
DARKATOS <alejoved@yahoo.com.co>
2015-10-05 22:10:54 -0500

    modificacion 05-10/10:10

--- vista/js/AdmAdministradorSipa.js
+++ vista/js/AdmAdministradorSipa.js
@@ -7,7 +7,7 @@ $(function () {
     function gConvenios(){
         $("#contenido").html('');
         $("#contenido").load("vista/html/AdmAdminCRUDgConvenios.html");
-        $("#banner").css("background",'transparent url("../../sipa/vista/images/fformulario.png") no-repeat fixed 0% 0% / cover')
+        $("#banner").css("background",'transparent url("../../ProyectoSIPA/vista/images/fformulario.png") no-repeat fixed 0% 0% / cover')
     }
     
     function aPracticas(){
